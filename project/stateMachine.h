7114640c94651bfe657cc2c3f700c01bfbbf355b
Nancy Hernandez <nhernandez28@miners.utep.edu>
2020-11-29 21:37:35 -0700

    added dim 25, dim 50, dim 75 and methods that turn red and green leds on and off

---
+++ project/stateMachine.h
@@ -0,0 +1,12 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+void dimAt25();
+void dimAt50();
+void dimAt75();
+void rLedOff();
+void rLedOn();
+void gLedOn();
+void tune();
+
+#endif
