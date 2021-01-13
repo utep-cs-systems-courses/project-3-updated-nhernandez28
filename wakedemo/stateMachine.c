95be72d3a76472beda619278460dd02b9375685d
Nancy Hernandez <nhernandez28@miners.utep.edu>
2021-01-12 22:21:09 -0700

    error with state_advance.c

--- wakedemo/stateMachine.c
+++ wakedemo/stateMachine.c
@@ -7,7 +7,7 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 
-char button_state = 0;
+char dim_select = 0;
 short int state = 0;
 short int dim = 0;
 short freq = 300;
@@ -62,95 +62,21 @@ char toggle_green()
   return changed;
 }
 
-char state1()
-{
-  static short curr = 0;
-  
-  switch(curr){
-  case 0:
-    red_on = 1;
-    state = 1;
-    curr = 1;
-    break;
-  case 1:
-    red_on = 0;
-    state = 0;
-    curr = 0;
-    break;
-  }
-
-  return 1;
-}
-
-char state2()
+char blink_red()
 {
   static char state = 0;
   switch(state){
   case 0:
     green_on = 1;
     state = 1;
-    buzzer_set_period(1000);
     break;
   case 1:
-    green_on = 0;
+    green_on = 1;
     state = 0;
-    buzzer_set_period(0);
     break;
   }
 
   return 1;
-}
-
-char state3()
-{
-  char changed = 0;
-  static enum {R=0, G=1} color = G;
-  switch(color){
-  case R:
-    changed = toggle_red();
-    color = G;
-    buzzer_set_period(2000);
-    break;
-  case G:
-    changed = toggle_green();
-    color = R;
-    buzzer_set_period(1000);
-    break;
-  }
-
-  changed_led = changed;
-  led_update();
-}
-
-char state4()
-{
-  red_on = 0;
-  changed_led = 1;
-  led_update();
-  dim75();
-  return 1;
-}
-
-void dim_advance()
-{
-  switch(dim){
-  case 0:
-    red_on = 1;
-    changed_led = 1;
-    led_update();
-    break;
-  case 1:
-    dim25();
-    break;
-  case 2:
-    dim50();
-    break;
-  case 3:
-    dim75;
-    break;
-  default:
-    break;
-  }
 }
 
 void dim25()
@@ -211,6 +137,32 @@ void dim75()
   led_update();
 }
 
+char dim_lights()
+{
+  switch(dim_select){
+  case 0:
+    red_on = 0;
+    changed_led = 1;
+    led_update();
+    dim_select = 1;
+    break;
+  case 1:
+    dim25();
+    dim_select = 2;
+    break;
+  case 2:
+    dim50();
+    dim_select = 3;
+    break;
+  case 3:
+    dim75();
+    dim_select = 0;
+    break;
+  }
+
+  return 1;
+}
+
 void buzzer_advance()
 {
   static char count = 0;
@@ -241,17 +193,17 @@ void state_advance()
 {
   char changed = 0;
   switch(switch_state_changed){
-  case 1:
+  case 0:
     changed = toggle_green();
     break;
+  case 1:
+    changed = toggle_red();
+    break;
   case 2:
-    changed = state2();
+    changed = blink_red();
     break;
   case 3:
-    changed = state3();
-    break;
-  case 4:
-    changed = state4();
+    changed = dim_lights();
     break;
   }
 
@@ -263,61 +215,3 @@ short get_period(short freq)
 {
   return 2000000/freq;
 }
-
-/*void diamondShape()
-{
-  switch(state){
-  case 0:
-    clearScreen(COLOR_BLACK);
-    drawDiamond(40, 100, 10, COLOR_PINK);
-    state = 1;
-    break;
-  case 1:
-    clearScreen(COLOR_BLACK);
-    drawDiamond(50, 100, 10, COLOR_PINK);
-    state = 2;
-    break;
-  case 2:
-    clearScreen(COLOR_BLACK);
-    drawDiamond(60, 100, 10, COLOR_PINK);
-    state = 3;
-    break;
-  case 3:
-    clearScreen(COLOR_BLACK);
-    drawDiamond(70, 100, 10, COLOR_PINK);
-    state = 4;
-    break;
-  case 4:
-    clearScreen(COLOR_BLACK);
-    drawDiamond(80, 100, 10, COLOR_PINK);
-    state = 1;
-    break;
-  }
-}
-
-void changeColors()
-{
-  switch(state){
-  case 1:
-    clearScreen(COLOR_BLACK);
-    drawDiamond(55, 80, 10, COLOR_GREEN);
-    state = 2;
-    break;
-  case 2:
-    clearScreen(COLOR_BLACK);
-    drawDiamond(55, 80, 10, COLOR_PINK);
-    state = 3;
-    break;
-  case 3:
-    clearScreen(COLOR_BLACK);
-    drawDiamond(55, 80, 10, COLOR_GREEN);
-    state = 4;
-    break;
-  case 4:
-    clearScreen(COLOR_BLACK);
-    drawDiamond(55, 80, 10, COLOR_PINK);
-    state = 1;
-    break;
-  }
-}
-*/
